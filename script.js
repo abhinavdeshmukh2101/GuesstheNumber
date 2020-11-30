b869347fbc88ed112eca2de6a212aa21f5bcd259
abhinavdeshmukh2101 <63723008+abhinavdeshmukh2101@users.noreply.github.com>
2020-12-01 00:48:49 +0530

    1st change

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
-//var->global scope
-// let and const -> block scope
+var correctnumber;
+var turn = 0;
+var hist = [];
 
-var correctnumber = Math.floor((Math.random * 100)+1);   // number which needs to be correctly guess by user
- 
-var flag = false;
 
-var guess;  // variable guess number that will be entered by user
+function startgame(correctnumber,turn){
+    correctnumber = Math.floor((Math.random() * 100)+1);
+    console.log(correctnumber);
+    document.getElementById("correct").style.display = "none";
+    document.getElementById("less").style.display = "none";
+    document.getElementById("correct").style.display = "none";
+    document.getElementById("start").style.display = "block";
+    document.getElementById("check").onclick = function() {checknumber(correctnumber,turn)};
+}
 
-function checknum(){
-    let x = document.getElementById("guess").value;
-    if(x == 4){
-        document.getElementById("correct").style.display = "block";
-        document.getElementById("less").style.display = "none";
-        document.getElementById("greater").style.display = "none";
-    }
-    else if(x < 4){
-        document.getElementById("correct").style.display = "none";
-        document.getElementById("greater").style.display = "none";
-        document.getElementById("less").style.display = "block";
-    }
-    else if(x > 4){
-        document.getElementById("correct").style.display = "none";
-        document.getElementById("less").style.display = "none";
-        document.getElementById("greater").style.display = "block";
-    }
+function times(turn){
+    if(document.getElementById("check").onclick = true)turn++;
 }
 
 
-function checknumber(){
-    let t = document.getElementById("guess").value;
-    if(t>100 || t<1){
+function checknumber(correctnumber,turn){
+    document.getElementById("start").style.display = "none";
+
+    let tempo = document.getElementById("guess").value;
+
+    if((Number(tempo)>100 || Number(tempo)<1) || Number(tempo) == null){
         document.write("Please enter a number between 1 to 100");
     }
-    checknum();
-    let temp = "Your entered " + t;
-    document.getElementById("history").innerHTML = temp;
+    
+
+    if(Number(tempo) == correctnumber){
+        document.getElementById("less").style.display = "none";
+        document.getElementById("greater").style.display = "none";
+        document.getElementById("start").style.display = "none";
+        document.getElementById("correct").style.display = "block";
+        //alert("You finished the game in " + turn + " turns. Hope you like the game!");
+
+    }
+    else if(Number(tempo) < correctnumber){
+        document.getElementById("correct").style.display = "none";
+        document.getElementById("greater").style.display = "none";
+        document.getElementById("start").style.display = "none";
+        document.getElementById("less").style.display = "block";
+    }
+    else if(Number(tempo) > correctnumber){
+        document.getElementById("correct").style.display = "none";
+        document.getElementById("less").style.display = "none";
+        document.getElementById("start").style.display = "none";
+        document.getElementById("greater").style.display = "block";
+    }
+
+    let temp = "Your entered " + tempo;
+    document.getElementById("history").style.innerText = temp;
+    document.getElementById('history').style.display = "block";
 }
+
